28d61aa79e336497b4bbf6ba304c321a0e5f1249
CYQ <chengyq2006@163.com>
2025-03-04 09:47:02 +0800

    main: 添加main函数与日志；修改小波代码

---
+++ utils/train.py
@@ -0,0 +1,12 @@
+class train_utils:
+    def __init__(self, args):
+        self.args = args
+    
+    def setup(self):
+        pass
+    
+    def train(self):
+        pass
+    
+    def plot(self):
+        pass
