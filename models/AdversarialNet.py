56c57973b1227c2ec76bb5ce9cf5aaba6f5cb7f3
CYQ <chengyq2006@163.com>
2025-03-11 17:57:39 +0800

    feat: 对抗网络

--- models/AdversarialNet.py
+++ models/AdversarialNet.py
@@ -2,7 +2,7 @@ from torch import nn
 import numpy as np
 
 
-def calc_coeff(iter_num, high=1.0, low=0.0, alpha=10.0, max_iter=10000.0):
+def calc_coeff(iter_num: int, high: float, low: float, alpha: float, max_iter: float) -> np.float64:
     '''
     动态计算对抗训练中的权重系数以调整梯度反转的强度
     Args:
@@ -17,7 +17,7 @@ def calc_coeff(iter_num, high=1.0, low=0.0, alpha=10.0, max_iter=10000.0):
     return np.float64(2.0 * (high - low) / (1.0 + np.exp(-alpha * iter_num / max_iter)) - (high - low) + low)
 
 
-def grl_hook(coeff):
+def grl_hook(coeff: float) -> function:
     '''
     实现GRL(Gradient Reverse Layer 梯度反转层)
     Args:
@@ -30,12 +30,32 @@ def grl_hook(coeff):
     return fun1
 
 class AdversarialNet(nn.Module):
-    def __init__(self, in_feature, hidden_size,max_iter=10000.0, trade_off_adversarial='Step', lam_adversarial=1.0):
-        super(AdversarialNet, self).__init__()
+    '''
+    对抗网络, 用于减小域间差异
+    Args:
+        in_feature(_int_): 输入特征维数
+        hidden_size(_int_): 隐藏层维数
+        trade_off_adversarial(_float_): 梯度反转层权重选择静态or动态更新
+        lam_adversarial(_float_): 静态反转时梯度所乘的系数
+        high(_float_): 轮次较大时系数接近high
+        low(_float_): 轮次较小时系数接近low
+        alpha(_float_): 随轮次增加时系数的衰减参数
+        max_iter(_float_): 最大迭代轮数, 影响梯度衰减因子
+    '''
+    def __init__(self, 
+                 in_feature: int, 
+                 hidden_size: int, 
+                 trade_off_adversarial: str = 'Step', 
+                 lam_adversarial: float = 1.0,
+                 high: float = 1.0,
+                 low: float = 0.0,
+                 alpha: float = 10.0,
+                 max_iter: float = 10000.0):
+        super().__init__() # 继承nn.Module的参数
         self.ad_layer1 = nn.Sequential(
             nn.Linear(in_feature, hidden_size),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
+            nn.ReLU(inplace=True), # mark: 此处为节省内存，采用了inplace操作
+            nn.Dropout()
         )
         self.ad_layer2 = nn.Sequential(
             nn.Linear(hidden_size, hidden_size),
@@ -44,19 +64,23 @@ class AdversarialNet(nn.Module):
         )
         self.ad_layer3 = nn.Linear(hidden_size, 1)
         self.sigmoid = nn.Sigmoid()
-        # parameters
-        self.iter_num = 0
-        self.alpha = 10
-        self.low = 0.0
-        self.high = 1.0
+        
+        # 参数
+        self.high = high
+        self.low = low
+        self.alpha = alpha
         self.max_iter = max_iter
         self.trade_off_adversarial = trade_off_adversarial
         self.lam_adversarial = lam_adversarial
-        self.__in_features = 1
+        self.iter_num = 0 # 当前迭代轮数
+        self.__in_features = 1 # 输出特征维数（伪私有化变量）
 
     def forward(self, x):
+        # 训练模式下，迭代轮次+1
         if self.training:
             self.iter_num += 1
+            
+        # 计算对抗损失权重，可选动态更新还是保持常数
         if self.trade_off_adversarial == 'Cons':
             coeff = self.lam_adversarial
         elif self.trade_off_adversarial == 'Step':
@@ -64,7 +88,7 @@ class AdversarialNet(nn.Module):
         else:
             raise Exception("loss not implement")
         x = x * 1.0
-        x.register_hook(grl_hook(coeff))
+        x.register_hook(grl_hook(coeff)) # 反转对抗层之前的梯度，以保证最大化领域对抗损失
         x = self.ad_layer1(x)
         x = self.ad_layer2(x)
         y = self.ad_layer3(x)
